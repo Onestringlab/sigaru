9c944669c2dbdb870141e9d93a2f5618a0f3fd9e
yulrio <33221012@std.stei.itb.ac.id>
2024-12-26 15:12:46 +0700

    Update footer

--- application/views/view_footer.php
+++ application/views/view_footer.php
@@ -1,9 +1,8 @@
-
 <div class="panel-footer  navbar-fixed-bottom">
     <span class="navbar-left">&nbsp;Sistem Informasi Penggunaan Ruangan</span>
     <span class="navbar-right">
         <img src="<?php echo base_url(); ?>images/logoUMP.png" width="20px" align="left">
-        &nbsp;Universitas Muhammadiyah Pontianak &copy; 2017
+        &nbsp;Universitas Muhammadiyah Pontianak &copy; 2024
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
     </span>
 </div>
