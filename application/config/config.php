0c2125858e4faf21a08cd0292ebda4b89228d33f
yulrio <33221012@std.stei.itb.ac.id>
2024-12-27 16:31:11 +0700

    Update config

--- application/config/config.php
+++ application/config/config.php
@@ -24,8 +24,8 @@ date_default_timezone_set('Asia/Jakarta');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:3300';
-// $config['base_url'] = 'https://sigaru.xyz';
+// $config['base_url'] = 'http://localhost:3300';
+$config['base_url'] = 'https://sigaru.xyz';
 
 /*
 |--------------------------------------------------------------------------
